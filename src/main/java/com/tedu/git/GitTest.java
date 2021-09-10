58a5da6f67011cf39eee64ff9e0763b11c7bfca6
junmuxian <517220725@qq.com>
2021-09-10 16:45:50 +0000

    master

--- src/main/java/com/tedu/git/GitTest.java
+++ src/main/java/com/tedu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("hot-fix");
+        System.out.println("master");
     }
 }
